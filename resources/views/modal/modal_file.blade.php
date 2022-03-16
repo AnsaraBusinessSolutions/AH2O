2946e6a45ffe7ce9a7864e27e182ec285623cddb
abs-bsamy-dev <96981764+abs-bsamy-dev@users.noreply.github.com>
2022-03-16 11:55:10 +0530

    modal dialog changes commit

---
+++ resources/views/modal/modal_file.blade.php
@@ -0,0 +1,17 @@
+<div class="modal " id="dataAlterModal" aria-hidden="true" aria-labelledby="dataAlterModalLabel" tabindex="-1">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="dataAlterModalLabel">Modal title</h5>
+          <button type="button" class="btn-close btn btn-primary" data-dismiss="modal" aria-label="Close">Close</button>
+        </div>
+        <div class="modal-body">
+          <p>Modal body text goes here.</p>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal" name="close">Close</button>
+          <button type="button" class="btn btn-primary" name="save">Save changes</button>
+        </div>
+      </div>
+    </div>
+  </div>
